57294779bdfb9c3198a51a8cd10a90141f2e3125
Sam Ostrom <sostrom@gmail.com>
2021-03-01 14:11:22 -0800

    more work

--- linked_lists/index.js
+++ linked_lists/index.js
@@ -5,3 +5,61 @@ class Node {
     }
 }
 
+class LinkedList {
+    constructor(){
+        this.head = null
+        this.size = 0;
+
+    }
+    // Insert first node
+    insertFirst(data) {
+        let node = new Node(data)
+        let current;
+        current = this.head
+        this.head = node
+        this.head.next = current
+        this.size++
+    }
+    // Insert last node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+        if(!this.head){
+            this.head = node;
+        } else {
+            current = this.head;
+            while(current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+        this.size++
+    }
+    // Insert at index
+
+    // Get at index
+
+    // remove at index
+
+    // clear list
+
+    // print list data
+    printListData(){
+        let current = this.head;
+
+        while(current) {
+            console.log(current.data)
+            current = current.next
+        }
+    }
+}
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400)
+
+ll.printListData()
